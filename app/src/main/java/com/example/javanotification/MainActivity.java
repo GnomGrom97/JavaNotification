a3810a52547b963ce718aa5d9b61b6ed682bfda6
GnomGrom97 <77610135+GnomGrom97@users.noreply.github.com>
2024-07-23 00:46:19 +0400

    Оповещение для чата 1

--- app/src/main/java/com/example/javanotification/MainActivity.java
+++ app/src/main/java/com/example/javanotification/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         //уведомление
-        NotificationChannel channel =null ;
+        NotificationChannel channel = null ;
         if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O) {
          channel =new NotificationChannel(
                  "TEST_CHANNEL",
